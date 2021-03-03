885402cf09b60b0de0d7a84dec10f8f6c3dbff3e
Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
2021-03-03 15:02:00 -0500

    Frontend Rehaul To Homepage and Contactpage

--- dpi-frontend/src/app/homepage/carousel/carousel.component.ts
+++ dpi-frontend/src/app/homepage/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-carousel',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  constructor() { }
+  constructor(private config : NgbCarouselConfig) {
+
+    config.interval = 2000;
+    config.wrap = true;  
+    config.keyboard = false;  
+    config.pauseOnHover = false; 
+
+   }
 
   ngOnInit(): void {
   }
