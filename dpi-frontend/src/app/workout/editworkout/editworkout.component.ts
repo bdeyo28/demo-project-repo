0b3c22d1d64f7c74ea3d99f18c19d461a2be1b5b
Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
2021-03-05 16:02:39 -0500

    Finished Adding Function

--- dpi-frontend/src/app/workout/editworkout/editworkout.component.ts
+++ dpi-frontend/src/app/workout/editworkout/editworkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Exercise } from 'src/app/Exercise';
+import { Intensity } from 'src/app/Intensity';
+import { IntensityService } from 'src/app/intensity.service';
 import { Workout } from 'src/app/Workout';
 import { WorkoutService } from 'src/app/workout.service';
 
@@ -15,14 +17,28 @@ export class EditworkoutComponent implements OnInit {
   @Input('ngModel')name : string;
   @Input('ngModel')intensityID : number;
   @Input('ngModel')desc : string;
+  @Input('ngModel')intensityName : string;
   completed : boolean;
   exerciseList : Exercise[];
+  intensityList : Intensity[];
+  intensity1 : string;
+  intensity2 : string;
+  intensity3 : string;
 
   constructor(private service : WorkoutService, 
               private router : Router,
-              private activatedRoute : ActivatedRoute) { }
+              private activatedRoute : ActivatedRoute,
+              private intService : IntensityService) { }
 
   ngOnInit(): void {
+
+    this.intService.getIntensityList().subscribe(list => {
+      this.intensityList = list;
+      this.intensity1 = this.intensityList[0].intensityName;
+      this.intensity2 = this.intensityList[1].intensityName;
+      this.intensity3 = this.intensityList[2].intensityName;
+    });
+
   }
 
   addWorkout() {
@@ -33,7 +49,7 @@ export class EditworkoutComponent implements OnInit {
   }
 
   deleteWorkout() {
-
+      
   }
 
 }
