e8118ca9685a4b6b940d93a1464166d3d220d7ba
Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
2021-03-04 21:11:15 -0500

    Implemented Videos Function

--- dpi-frontend/src/app/exercise/exercisedetails/exercisedetails.component.ts
+++ dpi-frontend/src/app/exercise/exercisedetails/exercisedetails.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, UrlSegment, UrlTree } from '@angular/router';
 import { Exercise } from 'src/app/Exercise';
 import { ExerciseService } from 'src/app/exercise.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Url } from 'url';
+
 
 @Component({
   selector: 'app-exercisedetails',
@@ -12,9 +13,11 @@ import { Url } from 'url';
 })
 export class ExercisedetailsComponent implements OnInit {
 
-  @Input()selectExercise : Exercise;
+  selectExercise : Exercise;
   exerciseID : number;
   safeURL : SafeResourceUrl;
+  videoURL : UrlTree;
+  urlPath : string;
 
   constructor(private service : ExerciseService, 
     private activatedRoute : ActivatedRoute, 
@@ -26,14 +29,19 @@ export class ExercisedetailsComponent implements OnInit {
     let idName : string = this.activatedRoute.snapshot.paramMap.get("exerciseID");
     this.exerciseID = parseInt(idName);
 
-    // let urlPath : string = this.router.snapshot.paramMap.get("exerciseURL");
 
-    // let videoURL : Url = this.router.parseURL()
+    this.service.getExerciseByID(this.exerciseID).subscribe(exercise => {this.selectExercise = exercise; 
+        this.urlPath = exercise.exerciseURL;
+        this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlPath)});
 
-    // this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(videoURL);
+    // console.log(this.selectExercise);
+    // this.urlPath = this.selectExercise.exerciseURL;
 
-    this.service.getExerciseByID(this.exerciseID).subscribe(id => {this.selectExercise = id});
+    // this.urlPath.replace("watch?=", "embed/");
 
+    // this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/dZgVxmf6jkA");
+
+    // this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlPath);
 
   }
 
